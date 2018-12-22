95e657dbc4df4c1fca92b9090c704f1ac0a4deee
TSTank <m18903577547@163.com>
2018-12-22 08:42:41 +0800

    这是我进行了修改加了一个int类型的参数

--- src/main/java/com/tt/spr/pojo/Student.java
+++ src/main/java/com/tt/spr/pojo/Student.java
@@ -2,6 +2,7 @@ package com.tt.spr.pojo;
 
 public class Student {
     private String studentName;
+    private int a=123;
 
     public String getStudentName() {
         return studentName;
@@ -9,5 +10,6 @@ public class Student {
 
     public void setStudentName(String studentName) {
         this.studentName = studentName;
+
     }
 }
